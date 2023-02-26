07b43eefb588d9378420c7a824f6844f06dfa505
RAFAELLOOR <rafaello_or@hotmail.com>
2023-02-25 19:16:23 -0600

    Homework 06

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -31,7 +31,7 @@ function nuevoUsuario(nombre, email, password) {
 function agregarPropiedad(objeto, propiedad) {
    // Recibirás un objeto por parámetro.
    // Debes agregarle una propiedad con el nombre recibido por parámetro.
-   // Esta propiedad será igual al valor `null`.
+   // Esta propiedad será igual al valor 'null'.
    // Retornar el objeto.
    // Tu código:
    objeto[propiedad]= null;
